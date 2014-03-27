1a41ddaac91e2e0cdad1ef849f85cf0c2eeb970b
David Charte <fdavidcl@outlook.com>
2014-03-27 12:09:01 +0100

    CardDealer

---
+++ CardDealer.rb
@@ -0,0 +1,42 @@
+#!/usr/bin/env ruby
+#encoding: utf-8
+
+require "singleton"
+
+module Game
+    class CardDealer
+    	include Singleton
+
+    	def initialize
+    	end
+
+    	private
+    	def init_treasure_card_deck
+    	end
+
+    	def init_monster_card_deck
+    	end
+
+    	def shuffle_treasures
+    	end
+
+    	def shuffle_monsters
+    	end
+
+    	public
+    	def next_treasure
+    	end
+
+    	def next_monster
+    	end
+
+    	def give_treasure_back(t)
+    	end
+
+    	def give_monster_back(m)
+    	end
+
+    	def init_cards
+    	end
+	end
+end
